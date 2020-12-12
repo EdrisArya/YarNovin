7fc0211117feb11a368aa58aa18e83fc7fd52143
M.A Felegary <edrisArya.iran@gmail.com>
2020-12-12 14:05:43 -0800

    add script file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-let sildes = document.querySelectorAll(".top-slider .slide");
+let sildes = document.querySelectorAll(".slide");
 let slideCounter=0;
 document.getElementById("next-btn").addEventListener("click",()=>{
-slideCounter++;
-console.log(sildes);
+if(slideCounter<sildes.length-1){
+    slideCounter++;
+    setTimeout(()=>{
+        console.log(slideCounter);
+        for(let i = 0;i<sildes.length;i++){
+            slides[i].
+        }
+    },1000)
+}else{
+    slideCounter=0;
+    setTimeout(()=>{
+        console.log(slideCounter);
+    },1000)
+}
 })
 document.getElementById("prev-btn").addEventListener("click",()=>{
     slideCounter--;
